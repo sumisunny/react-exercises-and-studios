3c74fa2eeb484dd5849088a1f50b3abd707d3b8c
ats <atsumitha@gmailcom>
2023-08-03 16:53:27 -0500

    react updated

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "The Best New Release Books You Must Read in 2023 and Beyond";
+   let book1 = "https://www.beyondthebookends.com/wp-content/uploads/2023/02/The-Little-Village-of-Book-Lovers-.jpg";
+   let book2 = "https://www.beyondthebookends.com/wp-content/uploads/2023/01/the-spectacular-.jpeg";
+   let book3 = "https://www.beyondthebookends.com/wp-content/uploads/2023/01/The-Only-one-left.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="The-Little-Village-of-Book-Lovers!"  height="400px" width="300px"/>
+         <img src={book2} alt="The-spectacular!" height="400px" width="300px"/>
+         <img src={book3} alt="The-Only-one-left!" height="400px" width="300px"/>
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,3 +1,14 @@
+import React from 'react';
+import styles from './ChoresList.module.css';
+
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   return (
+      <div className={styles.choresText}>
+   <p className={styles.choresHeading}>ChoresList</p>
+   <ol className={styles.choresList}>
+      <li>Cleaning</li>
+      <li>Washing</li>
+      <li>cooking</li>
+   </ol></div>
+   );
 }
